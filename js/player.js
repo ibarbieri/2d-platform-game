ef1dd36df8183604d23a2cd4ddf362c3a2eab01b
Ignacio Barbieri <barbieriignacio@gmail.com>
2014-04-06 00:38:57 -0300

    Background diagonal up

--- js/player.js
+++ js/player.js
@@ -1,15 +1,20 @@
 var canvasPlayer = document.getElementById('player'),
 	canvasBackground = document.getElementById('background'),
+	canvasExtras = document.getElementById('extras'),
 	contextPlayer = canvasPlayer.getContext('2d'),
 	contextBackground = canvasBackground.getContext('2d'),
+	contextExtras = canvasExtras.getContext('2d'),
 	onObstacle,
 	playerOnObstacle,
 	playerLife = $('#playerLife'),
+	playerScore = $('#playerScore'),
+	playerHeartsDragon = $('#playerHeartsDragon'),
 	controls = document.getElementById('controls'),
 	mobileJump,
 	mobileLeft,
 	mobileRight,
 	mobileAttack;
+
 
 
 (function (win) {
@@ -18,12 +23,12 @@ var canvasPlayer = document.getElementById('player'),
     win.requestAnimationFrame = requestAnimationFrame;
 
 	var canvasWidth = win.innerWidth,
-    	canvasHeight = win.innerHeight,
+    	canvasHeight = win.innerHeight + 200, // plus 230 to fix the space withe that leaves the rotation diagonal of the canvas
     	playerWidth,
     	playerHeight,
 		player = {
 			x : canvasWidth / 2,
-			y : canvasHeight - 140, // y position of the player
+			y : canvasHeight - 300, // y position of the player: 230 + the heigth of the player
 			playerWidth : 86,
 			playerHeight : 102,
 			speed: 4,
@@ -31,6 +36,8 @@ var canvasPlayer = document.getElementById('player'),
 			velY: 0,
 			jumping : false,
 			life : 5100,
+			score : 0,
+			heartsDragon : 0,
 			attack : false,
 			aggressive : 25
 		},
@@ -43,10 +50,19 @@ var canvasPlayer = document.getElementById('player'),
 			velX: 0,
 			velY: 0,
 			jumping : false
-		}
+		},
 		keys = [],
 		friction = 0.9,
 		gravity = 0.25;
+		// extras = {
+		// 	x : 0,
+		// 	y : 0,
+		// 	backgroundWidth : canvasWidth,
+		// 	backgroundHeight : canvasHeight,
+		// 	speed: 4,
+		// 	velX: 0,
+		// 	velY: 0,
+		// }
 
 	// Set the canvas dimentions equal to the window dimentions
 	canvasPlayer.width = canvasWidth;
@@ -54,6 +70,14 @@ var canvasPlayer = document.getElementById('player'),
 
 	canvasBackground.width = canvasWidth;
 	canvasBackground.height = canvasHeight;
+
+	canvasExtras.width = canvasWidth;
+	canvasExtras.height = canvasHeight;
+
+	// Rotate the background canvas
+	contextPlayer.rotate(-3.3 * Math.PI / 180);
+	contextBackground.rotate(-3.3 * Math.PI / 180);
+	contextExtras.rotate(-3.3 * Math.PI / 180);
 
 	// Create images objects to use in the game.
 	// ATTENTION, for testing, the images must have the url with ip. Does' t work with localhost
@@ -65,26 +89,37 @@ var canvasPlayer = document.getElementById('player'),
 
 		playerImage = new Image(),
 
-		rockObstacle = new Image();
+		smallRockObstacle = new Image(),
+		bigRockObstacle = new Image(),
+		waterObstacle = new Image(),
+
+		bigPlantExtra = new Image();
+
 
 	// backgrounds images
-	backgroundImage1.src = 'http://localhost/2d-platform-game/img/background-1.jpg';
+	backgroundImage1.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/background-1.jpg';
 	// backgroundImage1.onload = function () {
 	// 	// Create a pattern with this image, and set it to "repeat".
 	// 	backgroundPattern1 = contextBackground.createPattern(backgroundImage1, 'repeat');
 	// }
 
-	backgroundImage2.src = 'http://localhost/2d-platform-game/img/background-2.jpg';
+	backgroundImage2.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/background-1.jpg';
 
 	// enemies images
-	wolfImage.src = 'http://localhost/2d-platform-game/img/enemie.png';
-	warlockImage.src = 'http://localhost/2d-platform-game/img/warlock.png';
+	wolfImage.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/enemie.png';
+	warlockImage.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/warlock.png';
 
 	// player images
-	playerImage.src = 'http://localhost/2d-platform-game/img/player.png';
+	playerImage.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/player.png';
 
 	// obstacles images
-	rockObstacle.src = 'http://localhost/2d-platform-game/img/rock.png';
+	smallRockObstacle.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/small-rock.png';
+	bigRockObstacle.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/big-rock.png';
+
+	waterObstacle.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/water.png';
+
+	// extras images
+	bigPlantExtra.src = 'http://ibdesigns.com.ar/clients/lesath/game/img/big-plant.png';
 
 
 	/**
@@ -92,9 +127,9 @@ var canvasPlayer = document.getElementById('player'),
 	 * @function
 	 * @params {image, x, y, width, height}
 	 * @example
-	 * new Obstacles(rockObstacle, canvasWidth, canvasHeight -120, 150, 150);
+	 * new Obstacles(rockObstacle, canvasWidth, canvasHeight -120 -(the height of the image plus 230 of the canvas), 150, 150);
 	 */
-	function Obstacles (image, x, y, width, height) {
+	function Extras (image, x, y, width, height) {
 		this.image = image;
 		this.x = x;
 		this.y = y;
@@ -103,13 +138,40 @@ var canvasPlayer = document.getElementById('player'),
 	}
 
 	// Array to add and remove obstacles in the game
+	var extrasArray = [];
+
+	// Push obstacles into the array
+	extrasArray.push(
+				  new Extras(bigPlantExtra, canvasWidth + 300, canvasHeight - 310, 501, 188)
+				  //new Extras(waterObstacle, canvasWidth + 50, canvasHeight - 220, 222, 70)
+				  );
+
+	/**
+	 * Class constructor of obstacles
+	 * @function
+	 * @params {image, x, y, width, height}
+	 * @example
+	 * new Obstacles(rockObstacle, canvasWidth, canvasHeight -120 -(the height of the image plus 230 of the canvas), 150, 150);
+	 */
+	function Obstacles (name, image, x, y, width, height, differenceY) {
+		this.name = name;
+		this.image = image;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.differenceY = differenceY;
+	}
+
+	// Array to add and remove obstacles in the game
 	var obstaclesArray = [];
 
 	// Push obstacles into the array
 	obstaclesArray.push(
-				  new Obstacles(rockObstacle, canvasWidth, canvasHeight -146, 247, 110)
+				  new Obstacles('rock', smallRockObstacle, canvasWidth + 700, canvasHeight - 280, 159, 125, 50),
+				  new Obstacles('rock', bigRockObstacle, canvasWidth + 1100, canvasHeight - 302, 247, 194, 92),
+				  new Obstacles('water', waterObstacle, canvasWidth + 1500, canvasHeight - 220, 222, 70, 70)
 				  );
-
 
 	/**
 	 * Class constructor of enemies
@@ -136,12 +198,19 @@ var canvasPlayer = document.getElementById('player'),
 	var enemiesArray = [];
 
 	// Push enemies into the array
-	enemiesArray.push(
-				  new Enemies(400, 'wolf', wolfImage, canvasWidth, canvasHeight -80, 50, 50, 0, 0.9, 2, 50),
-				  new Enemies(400, 'wolf', wolfImage, canvasWidth, canvasHeight -80, 50, 50, 0, 0.9, 4, 50),
-				  new Enemies(400, 'wolf', wolfImage, canvasWidth + 800, canvasHeight -80, 50, 50, 0, 0.9, 3, 50),
-				  new Enemies(400, 'wolf', wolfImage, canvasWidth + 1400, canvasHeight -80, 50, 50, 0, 0.9, 2, 50)
-				  );
+	function pushEnemies () {
+		enemiesArray.push(
+					  new Enemies(400, 'wolf', wolfImage, canvasWidth + (-(background.x)), canvasHeight -250, 50, 50, 0, 0.9, 2, 50)
+					  );
+	}
+
+	// Add random enemies
+	var randomTimeEnemies = Math.floor(Math.random() * (7000 - 4000 + 1)) + 4000;
+
+	setInterval(function() {
+		pushEnemies();
+	}, randomTimeEnemies);
+
 
     /**
 	 * Move the players and the enemies and update it with requesAnimationFrame.
@@ -151,9 +220,6 @@ var canvasPlayer = document.getElementById('player'),
 	 * update();
 	 */
 	function update () {
-		// reset the player attack to false when the user drop the key a
-		player.attack = false;
-
 		// up arrow or space
 		if (keys[38] || keys[32] || mobileJump) {
 			onObstacle = false;
@@ -178,6 +244,9 @@ var canvasPlayer = document.getElementById('player'),
 				background.velX--;
 			}
 		}
+
+		// reset the player attack to false when the user drop the key a
+		player.attack = false;
 
 		// a key
 		if (keys[65] || mobileAttack) {
@@ -218,11 +287,19 @@ var canvasPlayer = document.getElementById('player'),
 			addFinalEnemie();
 		}
 
+
 		// render all the game
 		renders();
 
+
+		var lengthEnemiesArray = enemiesArray.length,
+			lengthobstaclesArray = obstaclesArray.length,
+			j,
+			k,
+			h;
+
 		// move the enemies
-		for (var j = 0; enemiesArray.length > j; j++) {
+		for (j = 0; lengthEnemiesArray > j; j += 1) {
 			// Check if the velocityX is less that the speed. If this condition is true continuous substracting the velocityX.
 			if (enemiesArray[j].velocityX >- enemiesArray[j].speed) {
 				enemiesArray[j].velocityX--;
@@ -235,18 +312,31 @@ var canvasPlayer = document.getElementById('player'),
 
 		onObstacle = false;
 
-		// check the collision whit the enemies
-		for (var k = 0; enemiesArray.length > k; k++) {
+		// check the collision whit the enemies and if the enemie is out the canvas
+		for (k = 0; lengthEnemiesArray > k; k += 1) {
+
 			checkCollision(player, enemiesArray[k]);
+
+			// if (lengthEnemiesArray > 5) {
+			// 	enemiesIsOutTheCanvas(enemiesArray[k]);
+			// }
 		}
 
 		// check the collision with the obstacles
-		for (var h = 0; obstaclesArray.length > h; h++) {
-			checkCollision(player, obstaclesArray[h]);
+		for (h = 0; lengthobstaclesArray > h; h += 1) {
+			if (obstaclesArray[h].name == 'rock') {
+				checkCollision(player, obstaclesArray[h]);
+			}
 		}
 
-		// Update player's life
-		playerLife.html(player.life);
+		// Update player life
+		playerLife.html('PLAYER LIFE :' + player.life);
+
+		// Update player score
+		playerScore.html('PLAYER SCORE :' + player.score);
+
+		// Update player hearts dragon
+		playerHeartsDragon.html('PLAYER HEARTS DRAGON :' + player.heartsDragon);
 
 		// run through the loop again to refresh the game all time
 		requestAnimationFrame(update);
@@ -293,9 +383,12 @@ var canvasPlayer = document.getElementById('player'),
 	 * renderObstacles();
 	 */
 	function renderEnemies () {
-		var wolfImageDifference = canvasHeight - wolfImage.height;
 
-		for (var i = 0; enemiesArray.length > i; i++) {
+		var wolfImageDifference = canvasHeight - wolfImage.height,
+		lengthEnemiesArray = enemiesArray.length,
+		i;
+
+		for (i = 0; lengthEnemiesArray > i; i += 1) {
 			// plus this: enemiesArray[i].x + background.x: becouse i need to know all time where the background.x is and plus it to the positio of the enemie.
 			contextBackground.drawImage(
 				enemiesArray[i].image,
@@ -314,13 +407,41 @@ var canvasPlayer = document.getElementById('player'),
 	 * renderObstacles();
 	 */
 	function renderObstacles () {
-		for (var n = 0; obstaclesArray.length > n; n++) {
+
+		var lengthObstaclesArray = obstaclesArray.length,
+		n;
+
+		for (n = 0; lengthObstaclesArray > n; n += 1) {
 			contextBackground.drawImage(
 					obstaclesArray[n].image,
 					obstaclesArray[n].x + background.x,
 					obstaclesArray[n].y + background.y,
 					obstaclesArray[n].width,
 					obstaclesArray[n].height);
+		}
+	}
+
+	/**
+	 * Render all the extras in the game
+	 * @function
+	 * @example
+	 * renderExtras();
+	 */
+	function renderExtras () {
+		var lengthExtrasArray = extrasArray.length,
+		l;
+
+		// Clean the context para que no se vayan agregando una imagen arriba de la otra
+		contextExtras.clearRect(0, 0, canvasWidth, canvasHeight);
+
+		for (l = 0; lengthExtrasArray > l; l += 1) {
+			//console.log('extrasArray[l].x', extrasArray[l].x , 'background.x', background.x, 'position of extra', extrasArray[l].x + background.x);
+			contextExtras.drawImage(
+					extrasArray[l].image,
+					extrasArray[l].x + background.x,
+					extrasArray[l].y + background.y,
+					extrasArray[l].width,
+					extrasArray[l].height);
 		}
 	}
 
@@ -332,7 +453,7 @@ var canvasPlayer = document.getElementById('player'),
 	 */
 	function addFinalEnemie () {
 		enemiesArray.push(
-				new Enemies(100, 'warlock', warlockImage, backgroundImage1.width + backgroundImage2.width - warlockImage.width, canvasHeight -275, 245, 258, 0, 0, 0, 150)
+				new Enemies(100, 'warlock', warlockImage, backgroundImage1.width + backgroundImage2.width - warlockImage.width, canvasHeight -508, 245, 258, 0, 0, 0, 150)
 				);
 	}
 
@@ -364,25 +485,38 @@ var canvasPlayer = document.getElementById('player'),
 			if (oX >= oY) {
 				if (onObstacle == false) {
 					if (distanceToCollisionY > 0) {
-						collisionDirection = "button";
+						collisionDirection = "BUTTON";
 					} else {
 						// Block de background
-						collisionDirection = "top";
+						collisionDirection = "TOP";
 
-						background.y = enemieOrObstacle.height;
-						onObstacle = true;
+						// if the collision come from wolf
+						if (enemieOrObstacle.name == 'wolf') {
 
+							background.y = enemieOrObstacle.height;
+
+						// when te collision is with obstacle
+						} else if (enemieOrObstacle.name == 'rock') {
+
+							background.y = enemieOrObstacle.height - enemieOrObstacle.differenceY;
+							onObstacle = true;
+						}
+												// } else if (enemieOrObstacle.name == 'water') {
+						// 	//console.log('estoy sobre water');
+						// 	background.velX -= 0.5;
+						// 	onObstacle = true;
+						// }
 					}
 				}
 
 			} else {
 				if (distanceToCollisionX > 0) {
 					// Block de background
-					collisionDirection = "left";
+					collisionDirection = "LEFT";
 					background.velX = 0;
 					background.velX--;
 
-					// if the collision come from enemie
+					// if the collision come from wolf
 					if (enemieOrObstacle.name == 'wolf') {
 
 						// if the player is attacking
@@ -412,11 +546,17 @@ var canvasPlayer = document.getElementById('player'),
 
 						// check if the player or enemie is alive
 						entityIsAlive(player, enemieOrObstacle);
-					};
+
+					}
+					// else if (enemieOrObstacle.name == 'water') {
+ 				// 		console.log('left');
+ 				// 		onObstacle = true;
+					// }
+
 
 				} else {
 					// Block de background
-					collisionDirection = "right";
+					collisionDirection = "RIGHT";
 					background.velX = 0;
 					background.velX++;
 
@@ -449,7 +589,12 @@ var canvasPlayer = document.getElementById('player'),
 
 						// check if the player or enemie is alive
 						entityIsAlive(player, enemieOrObstacle);
-					};
+
+ 					}
+ 				// 	else if (enemieOrObstacle.name == 'water') {
+ 				// 		background.velX -= 0.5;
+ 				// 		onObstacle = true;
+					// }
 
 				}
 			}
@@ -468,6 +613,9 @@ var canvasPlayer = document.getElementById('player'),
 	 * checkCollision(player, enemies);
 	 */
 	function entityIsAlive (player, enemies) {
+
+		var enemiesArrayPosition = enemiesArray.indexOf(enemies);
+
 		if (player.life < 0) {
 
 			console.log('GAME OVER');
@@ -477,14 +625,28 @@ var canvasPlayer = document.getElementById('player'),
 			// Enemies and obstacles reset the array.
 			// Life player set to 100%.
 
-
 		} else if (enemies.life < 0) {
-
 			//get the position of the enemie died;
 			var enemiesArrayPosition = enemiesArray.indexOf(enemies);
-
 			//remove the enemie died;
 			enemiesArray.splice(enemiesArrayPosition, 1);
+
+			// The player get 100 points becouse kill one enemie
+			playerUpdateScore(100);
+		}
+	}
+
+	function playerUpdateScore (scoreGet) {
+		player.score += scoreGet;
+	}
+
+	function enemiesIsOutTheCanvas (enemies) {
+		var positionXOfEnemie = enemiesArray[enemiesArray.indexOf(enemies)].x;
+		if (positionXOfEnemie < 100) {
+			//no me deja eliminarlo porque todavía esta chequeando si tiene colition
+			//enemiesArray.splice(enemiesArray[enemiesArray.indexOf(enemies)], 1);
+			console.log(enemiesArray.indexOf(enemies));
+			enemiesArray.splice(enemiesArray.indexOf(enemies), 1);
 		}
 	}
 
@@ -492,7 +654,7 @@ var canvasPlayer = document.getElementById('player'),
 	/* Sprite animation */
 	var playerSprite = new Image();
 
-		playerSprite.src = 'http://localhost/2d-platform-game/img/enemie.png';
+		playerSprite.src = 'http://localhost/2d-platform-game/img/player-animation.png';
 
 
 
@@ -516,7 +678,21 @@ var canvasPlayer = document.getElementById('player'),
 
 		// Call the function tha render the obstacles. I have to call this function random or when y want to a obstacle appear.
 		renderObstacles();
+
+		// Call the function tha render the extras.
+		renderExtras();
 	}
+
+
+	// Run the function renderExtras() each 9 seconds
+	// setInterval(function() {
+	// 	// Clean the context para que no se vayan agregando una imagen arriba de la otra
+	// 	contextExtras.clearRect(0, 0, canvasWidth, canvasHeight);
+
+	// 	// Call the function tha render the extras.
+	// 	renderExtras();
+	// }, 9000);
+
 
 	// ATENCION: VER SI SETEANDO ALGUN FLAGA ACA PUEDO ARREGLAR LO DE DEJAR DE SALTAR OBSTACLES
 	// listen when a key is pressed
